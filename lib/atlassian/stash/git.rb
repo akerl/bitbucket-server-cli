7a93d29cae0f0e7419b2572e134b8306becf833b
Seb Ruiz <sruiz@atlassian.com>
2012-10-25 13:32:26 +1100

    Support extracting reviewers from args

--- lib/atlassian/stash/git.rb
+++ lib/atlassian/stash/git.rb
@@ -4,18 +4,18 @@ module Atlassian
   module Stash
     module Git
       def get_current_branch
-      %x(git symbolic-ref HEAD)[/refs\/heads\/(.*)/, 1]
+        %x(git symbolic-ref HEAD)[/refs\/heads\/(.*)/, 1].chomp
       end
 
       def is_in_git_repository?
-        system('git rev-parse')
+        system('git rev-parse').chomp
       end
 
       def ensure_within_git!
         if is_in_git_repository?
           yield
         else
-          raise "You must execute this command in a git repository"
+          raise "fatal: Not a git repository"
         end
       end
     end
