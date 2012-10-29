1a32f289fa57bea25bd20f7f3a1069f744ee8871
Seb Ruiz <me@sebruiz.net>
2012-10-30 07:28:32 +1100

    Fully support project keys and repo slugs as they are in Stash, by using the proper regular expressions. Add tests

--- test/test_stash-create-pull-request.rb
+++ test/test_stash-create-pull-request.rb
@@ -6,16 +6,25 @@ include Atlassian::Stash::Git
 class TestStashCreatePullRequest < Test::Unit::TestCase
   
 	should "extract project key and repo slug from Stash remote" do
-		get_remote_url = "https://sruiz@stash-dev.atlassian.com/scm/STASH/stash.git"
+		remote = "https://sruiz@stash-dev.atlassian.com/scm/STASH/stash.git"
 	    cpr = CreatePullRequest.new nil
-	    ri = cpr.extract_repository_info
+	    ri = cpr.extract_repository_info remote
 	    assert_equal 'STASH', ri.projectKey
 	    assert_equal 'stash', ri.slug
 	end
 
-	# should "extracting project key and repo slug from non stash url raises exception" do
-	#     get_remote_url = "git@bitbucket.org:sebr/atlassian-stash-rubygem.git"
-	#     cpr = CreatePullRequest.new nil
-	#     assert_raise(RuntimeError) { cpr.extract_repository_info }
-	# end
+	should "extracting project key and repo slug from non stash url raises exception" do
+	    remote = "git@bitbucket.org:sebr/atlassian-stash-rubygem.git"
+	    cpr = CreatePullRequest.new nil
+	    assert_raise(RuntimeError) { cpr.extract_repository_info remote }
+	end
+
+
+	should "repo with hyphes" do
+		remote = "https://sruiz@stash-dev.atlassian.com/scm/s745h/stash-repository.git"
+	    cpr = CreatePullRequest.new nil
+	    ri = cpr.extract_repository_info remote
+	    assert_equal 's745h', ri.projectKey
+	    assert_equal 'stash-repository', ri.slug
+	end
 end
